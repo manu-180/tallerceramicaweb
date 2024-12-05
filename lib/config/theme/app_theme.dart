f0b5c4e62c163181518cba1b6918060a4f892a21
Manuel Navarro <manunv97@gmail.com>
2024-12-05 18:00:18 -0300

    ajustes de funcionamiento

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,21 +1,14 @@
 
 import 'package:flutter/material.dart';
-final List<Color> listColors = [
-  // Tonos cálidos (rojos, naranjas y rosas)
-  Color(0xFFE74C3C), // Rojo cereza
 
-  // Tonos amarillos y beige
-  Color(0xFFF7D794), // Beige
 
-  // Tonos verdes
-  Color(0xFF55E6C1), // Verde lima
-
-  // Tonos azules y aguamarina
-  Color(0xFF00CEC9), // Aguamarina
-  Color(0xFFADD8E6), // Azul claro
-
-  // Tonos púrpuras
-  Color(0xFFAF52DE), // Púrpura
+const List<Color> listColors = [
+  Color(0xFFE74C3C), 
+  Color(0xFFF7D794), 
+  Color(0xFF55E6C1), 
+  Color(0xFF00CEC9), 
+  Color(0xFFADD8E6), 
+  Color(0xFFAF52DE), 
 ];
 
 
