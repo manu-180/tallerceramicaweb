7dde9fa556cab2a1928088ca638a83488968a602
Manuel Navarro <manunv97@gmail.com>
2024-12-12 16:40:51 -0300

    porlas

---
+++ lib/supabase/functions/rutas_talleres.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RutasTalleres extends StatelessWidget {
+  const RutasTalleres({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Seleccione su taller'),
+      ),
+    );
+  }
+}
